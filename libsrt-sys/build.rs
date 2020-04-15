e83d1815c7acac4199d5994fa0018b336d732dab
Hieu Nguyen <nthieu173@gmail.com>
2020-04-15 12:09:27 -0400

    Complete build script

--- libsrt-sys/build.rs
+++ libsrt-sys/build.rs
@@ -1,23 +1,34 @@
+use std::env;
 use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    run_configure();
-}
-
-fn run_configure() {
     let mut tcl_shell = PathBuf::from("tclkit");
     if cfg!(target_os = "windows") {
         tcl_shell.push("tclkit-8.6.3-win32-ix86.exe");
     } else {
         tcl_shell.push("tclkit-8.6.3-rhel5-ix86");
     }
-    let mut configure = PathBuf::from("srt-src");
+    let mut configure = PathBuf::from("..");
+    configure.push("srt-src");
     configure.push("configure");
+    env::set_current_dir("libsrt").expect("failed to set current dir to libsrt");
     Command::new(tcl_shell)
-        .current_dir("libsrt")
         .arg(configure)
         .arg("--prefix")
+        .arg(".")
         .output()
-        .expect("failed to execute process");
+        .expect("failed to run configure");
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .arg("make install")
+            .output()
+            .expect("failed to run cmake");
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg("make install")
+            .output()
+            .expect("failed to run cmake");
+    }
 }
