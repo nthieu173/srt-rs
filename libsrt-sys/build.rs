daa9e4c9cb3efac422858c89ebae879e282761f4
Hieu Nguyen <nthieu173@gmail.com>
2020-04-16 17:43:36 -0400

    output libsrt to OUT_DIR

--- libsrt-sys/build.rs
+++ libsrt-sys/build.rs
@@ -3,17 +3,20 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    let mut tcl_shell = PathBuf::from("tclkit");
+    let build_dir = env::current_dir().unwrap();
+    let mut tcl_shell = build_dir.clone();
+    tcl_shell.push("tclkit");
     if cfg!(target_os = "windows") {
         tcl_shell.push("tclkit-8.6.3-win32-ix86.exe");
     } else {
         tcl_shell.push("tclkit-8.6.3-rhel5-ix86");
     }
-    let mut configure = PathBuf::from("..");
+    let mut configure = build_dir;
     configure.push("srt-src");
     configure.push("configure");
-    env::set_var("OUT_DIR", "libsrt");
-    env::set_current_dir("libsrt").expect("failed to set current dir to libsrt");
+    let out_dir = env::var("OUT_DIR").unwrap();
+    println!("{}", out_dir);
+    env::set_current_dir(&out_dir).expect("failed to set current dir to libsrt");
     Command::new(tcl_shell)
         .arg(configure)
         .arg("--prefix")
@@ -32,5 +35,5 @@ fn main() {
             .output()
             .expect("failed to run cmake");
     }
-    println!("cargo:rustc-link-search=/libsrt");
+    println!("cargo:rustc-link-search={}", out_dir);
 }
