e9ea18c3271fc1fc42eea1f93c113edcf6cfd966
Hieu Nguyen <nthieu173@gmail.com>
2020-04-20 12:22:44 -0400

    Give up building libsrt for windows

--- libsrt-sys/build.rs
+++ libsrt-sys/build.rs
@@ -1,38 +1,16 @@
 use std::{env, error::Error, process::Command};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let build_dir = env::current_dir()?;
-    let mut configure = build_dir.clone();
-    configure.push("srt-src");
-    configure.push("configure");
-    let out_dir = env::var("OUT_DIR")?;
-    env::set_current_dir(&out_dir).expect("failed to set current dir to libsrt");
     if cfg!(windows) {
-        let mut tcl_shell = build_dir;
-        tcl_shell.push("tclkit");
-        tcl_shell.push("tclkit-cli-8_6_10-twapi-4_3_8-x86-max.exe");
-        let mut configure_command = Command::new(tcl_shell);
-        configure_command
-            .arg(configure)
-            .arg("--enable-apps=OFF")
-            .arg(format!(
-                "--pthread-include-dir={}",
-                env::var("PTHREAD_INCLUDE_DIR")?
-            ))
-            .arg("--prefix=.");
-        if let Ok(openssl_include_dir) = env::var("OPENSSL_INCLUDE_DIR") {
-            configure_command.arg(format!("--openssl-include-dir={}", openssl_include_dir));
-        }
-        let output = configure_command.output().expect("failed to configure");
-        if !String::from_utf8(output.stdout)?.contains("Build files have been written to") {
-            panic!("failed to generate build files");
-        }
-        Command::new("cmd")
-            .arg("make install")
-            .output()
-            .expect("failed to run cmake");
-        println!("cargo:rustc-link-search={}", out_dir);
+        println!(
+            "cargo:rustc-link-search={}",
+            env::var("SRT_LIB_DIR").expect("SRT_LIB_DIR not set")
+        );
     } else {
+        let mut configure = PathBuf(env::current_dir()?);
+        configure.push("srt-src");
+        configure.push("configure");
+        env::set_current_dir(env::var("OUT_DIR")?).expect("failed to set current dir to libsrt");
         let output = Command::new("tclsh")
             .arg(configure)
             .arg("--enable-apps=OFF")
@@ -46,8 +24,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             .arg("-c")
             .arg("make install")
             .output()
-            .expect("failed to run cmake");
-        println!("cargo:rustc-link-search={}", out_dir);
+            .expect("failed to run cmake in sh");
+        println!("cargo:rustc-link-search={}", env::var("OUT_DIR")?);
     }
     Ok(())
 }
