ffccb7e7ab9a7615ea3bf28c51a405b9daa33d17
Hieu Nguyen <nthieu173@gmail.com>
2020-04-20 11:19:56 -0400

    Update cross platform build

--- libsrt-sys/build.rs
+++ libsrt-sys/build.rs
@@ -1,37 +1,53 @@
-use std::env;
-use std::process::Command;
+use std::{env, error::Error, process::Command};
 
-fn main() {
-    let build_dir = env::current_dir().unwrap();
-    let mut tcl_shell = build_dir.clone();
-    tcl_shell.push("tclkit");
-    if cfg!(windows) {
-        tcl_shell.push("tclkit-8.6.3-win32-ix86.exe");
-    } else {
-        tcl_shell.push("tclkit-8.6.3-rhel5-ix86");
-    }
-    let mut configure = build_dir;
+fn main() -> Result<(), Box<dyn Error>> {
+    let build_dir = env::current_dir()?;
+    let mut configure = build_dir.clone();
     configure.push("srt-src");
     configure.push("configure");
-    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir = env::var("OUT_DIR")?;
     env::set_current_dir(&out_dir).expect("failed to set current dir to libsrt");
-    Command::new(tcl_shell)
-        .arg(configure)
-        .arg("--prefix")
-        .arg(".")
-        .output()
-        .expect("failed to run configure");
     if cfg!(windows) {
+        let mut tcl_shell = build_dir;
+        tcl_shell.push("tclkit");
+        tcl_shell.push("tclkit-cli-8_6_10-twapi-4_3_8-x86-max.exe");
+        let mut configure_command = Command::new(tcl_shell);
+        configure_command
+            .arg(configure)
+            .arg("--enable-apps=OFF")
+            .arg(format!(
+                "--pthread-include-dir={}",
+                env::var("PTHREAD_INCLUDE_DIR")?
+            ))
+            .arg("--prefix=.");
+        if let Ok(openssl_include_dir) = env::var("OPENSSL_INCLUDE_DIR") {
+            configure_command.arg(format!("--openssl-include-dir={}", openssl_include_dir));
+        }
+        let output = configure_command.output().expect("failed to configure");
+        if !String::from_utf8(output.stdout)?.contains("Build files have been written to") {
+            panic!("failed to generate build files");
+        }
         Command::new("cmd")
             .arg("make install")
             .output()
             .expect("failed to run cmake");
+        println!("cargo:rustc-link-search={}", out_dir);
     } else {
+        let output = Command::new("tclsh")
+            .arg(configure)
+            .arg("--enable-apps=OFF")
+            .arg("--prefix=.")
+            .output()
+            .expect("failed to run configure in tclsh");
+        if !String::from_utf8(output.stdout)?.contains("Build files have been written to") {
+            panic!("failed to generate build files");
+        }
         Command::new("sh")
             .arg("-c")
             .arg("make install")
             .output()
             .expect("failed to run cmake");
+        println!("cargo:rustc-link-search={}", out_dir);
     }
-    println!("cargo:rustc-link-search={}", out_dir);
+    Ok(())
 }
